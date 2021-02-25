84b0905b7082066b21edd706ba795d8ddd35e1f5
Vinicius Moralis <vinicius_moralis@hotmail.com>
2021-02-25 20:24:11 -0300

    iniciando com context api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CompletedChallenges } from "../components/CompletedChallengs";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
